9bf7ee60c401f5ee46301be70a0998a80da38a70
Sanaye Lewis <sanayetalsk@gmail.com>
2020-06-08 13:52:04 -0400

    Complete Iteration 0

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Sanaye Lewis on 6/8/20.
+//  Copyright © 2020 Sanaye Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
